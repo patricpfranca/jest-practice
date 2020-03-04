6e66de9e96662237fede4edef833d26ef9268456
Patric Pereira <patric.pereirafranca@gmail.com>
2020-03-04 12:15:33 -0300

    testing array map

--- src/map/map.js
+++ src/map/map.js
@@ -2,7 +2,7 @@ const map = (arr, func) => {
   let newArr = [];
 
   for (let i = 0; i < arr.length; i++) {
-    newArr.push(func(arr[i], i));
+    newArr.push(func(arr[i], i, arr));
   }
 
   return newArr;
