e67bbe1e81b26989686cd7f53bca0e52032a568d
Patric Pereira <patric.pereirafranca@gmail.com>
2020-03-04 11:59:08 -0300

    testing index map

--- __tests__/map.test.js
+++ __tests__/map.test.js
@@ -16,3 +16,11 @@ it("map([3, 4], (item) => item) should return [3, 4]", () => {
 it("map([1, 2], (item) => item + 1) should return [2, 3]", () => {
   expect(map([1, 2], item => item + 1)).to.be.deep.equal([2, 3]);
 });
+
+it("map([4, 6], (item) => item + 1) should return [5, 7]", () => {
+  expect(map([4, 6], item => item + 1)).to.be.deep.equal([5, 7]);
+});
+
+it("map([1, 2], (item, index) => index) should return [0, 1]", () => {
+  expect(map([1, 2], (item, index) => index)).to.be.deep.equal([0, 1]);
+});
--- src/map/map.js
+++ src/map/map.js
@@ -1,7 +1,8 @@
 const map = (arr, func) => {
   let newArr = [];
+
   for (let i = 0; i < arr.length; i++) {
-    newArr.push(func(arr[i]));
+    newArr.push(func(arr[i], i));
   }
 
   return newArr;
