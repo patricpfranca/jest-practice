c4f6eebecd1d7f9eea80535d75c85151fe54fbc8
Patric Pereira <patric.pereirafranca@gmail.com>
2020-04-10 12:39:04 -0300

    reduce right recursive testing

---
+++ src/reduce/reduce-right-recursive.js
@@ -0,0 +1,6 @@
+import mainReduce from "./main-reduce-recursive";
+import reverse from "../reverse/reverse-recursive";
+
+const reduceRight = (arr, ...params) => mainReduce(reverse(arr), ...params);
+
+export default reduceRight;
